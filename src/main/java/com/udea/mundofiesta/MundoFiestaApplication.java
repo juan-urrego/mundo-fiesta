164409ec9613eefaddbae59e6ea2f1f96a0f404a
juan urrego <juan-urrego@live.com>
2022-02-22 16:10:23 -0500

    proyecto inicializado

---
+++ src/main/java/com/udea/mundofiesta/MundoFiestaApplication.java
@@ -0,0 +1,13 @@
+package com.udea.mundofiesta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MundoFiestaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MundoFiestaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/udea/mundofiesta/MundoFiestaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udea.mundofiesta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MundoFiestaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

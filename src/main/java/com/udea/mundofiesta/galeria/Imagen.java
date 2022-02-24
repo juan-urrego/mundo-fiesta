7a738a2ab2bf36087bfac04c6934442a9987b40b
juan urrego <juan-urrego@live.com>
2022-02-23 20:12:26 -0500

    servicio de cloud finalizado

--- src/main/java/com/udea/mundofiesta/galeria/Imagen.java
+++ src/main/java/com/udea/mundofiesta/galeria/Imagen.java
@@ -20,10 +20,23 @@ public class Imagen {
     @Column(name = "img_id")
     private Integer id;
 
+    @Column(name = "img_nombre", nullable = false, length = 50)
+    private String nombre;
+
     @Column(name = "img_url", nullable = false)
     private String imagenUrl;
+
+    @Column(name = "img_cloud_id", nullable = false)
+    private String cloudId;
 
     @ManyToOne(optional = false)
     @JoinColumn(name = "categoria_id", nullable = false)
     private Categoria categoria;
+
+    public Imagen(String nombre, String imagenUrl, String cloudId, Categoria categoria) {
+        this.nombre = nombre;
+        this.imagenUrl = imagenUrl;
+        this.cloudId = cloudId;
+        this.categoria = categoria;
+    }
 }
--- src/main/java/com/udea/mundofiesta/galeria/ImagenService.java
+++ src/main/java/com/udea/mundofiesta/galeria/ImagenService.java
@@ -2,12 +2,15 @@ package com.udea.mundofiesta.galeria;
 
 
 import com.udea.mundofiesta.producto.Producto;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface ImagenService {
     List<Imagen> getAll();
     List<Imagen> getAllByIdCategoria(Integer idCategoria);
-    Imagen save(Imagen imagen);
-    void delete(Integer id);
+    Imagen getById(Integer id);
+    Imagen save(MultipartFile multipartFile, Integer idCategoria) throws IOException;
+    void delete(Integer id) throws IOException;
 }

95304026808f0214981eb101162e63add0c822e6
juan urrego <juan-urrego@live.com>
2022-02-28 16:40:56 -0500

    salto de linea

--- src/main/java/com/udea/mundofiesta/usuario/RolServiceImpl.java
+++ src/main/java/com/udea/mundofiesta/usuario/RolServiceImpl.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-
 @Service
 @AllArgsConstructor
 public class RolServiceImpl implements RolService {

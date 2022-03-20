4dc348cff3846494a3708340720cd4ead0f3bec7
“Juan <juan-urrego@live.com>
2022-03-19 20:59:34 -0500

    removiendo configuracion de cors

--- src/main/java/com/udea/mundofiesta/MundoFiestaApplication.java
+++ src/main/java/com/udea/mundofiesta/MundoFiestaApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.*;
 
@@ -16,19 +14,6 @@ public class MundoFiestaApplication {
     public static void main(String[] args) {
         SpringApplication.run(MundoFiestaApplication.class, args);
     }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
-            }
-        };
-    }
-
 
     @Bean
     CommandLineRunner run(UsuarioService usuarioService, RolService rolService) {
